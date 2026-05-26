b664972b0f81db09e66b27299030103e4c209c63
Ronaldo da Rocha <ronaldodarocha@outlook.com.br>
2026-05-26 18:01:04 -0300

    alterado

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NEXT_PUBLIC_API_URL
+          ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+          : "http://127.0.0.1:80/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
